4a98cd67accbbe5a4db62424c917edf4ddd6d5be
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-04-18 21:28:46 -0500

    Added onclick prompt on html file for generate password button. Also added a prompt once you click the generate button.

--- script.js
+++ script.js
@@ -15,12 +15,21 @@ passwordText.value = password;
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function promptMe() {
+
+    var startprompt = Number(prompt("How many characters would you like your password to be?"));
+    while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+}
+
+
+
+
 
 //Questions for prompts
-var uppercase = confirm("Would you like to include uppercase letters?");
-var lowercase = confirm("Would you like to include lowercase letters?");
-var numbers = confirm("Would you like to include numbers?");
-var symbols = confirm("Would you like to include special characters?");
+//var uppercase = confirm("Would you like to include uppercase letters?");
+//var lowercase = confirm("Would you like to include lowercase letters?");
+//var numbers = confirm("Would you like to include numbers?");
+//var symbols = confirm("Would you like to include special characters?");
 
 
 
