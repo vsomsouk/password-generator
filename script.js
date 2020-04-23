47901ec07ad818e3c0f3805370654d3da2978045
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-04-22 22:47:24 -0500

    Added lower, upper, numbers and symbols for values. Started the for loop.

--- script.js
+++ script.js
@@ -18,15 +18,28 @@ generateBtn.addEventListener("click", writePassword);
 function promptMe() {
     var startprompt = Number(prompt("How many characters would you like your password to be?"));
     while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
-}
+
 
 
 // Questions for prompts
-//function myFunction() {
-  // var uppercase = confirm("Would you like to include uppercase letters?");
-  // var lowercase = confirm("Would you like to include lowercase letters?");
-  // var numbers = confirm("Would you like to include numbers?");
-  // var symbols = confirm("Would you like to include special characters?");
-// }
+
+  var uppercase = confirm("Would you like to include uppercase letters?");
+  var lowercase = confirm("Would you like to include lowercase letters?");
+  var numbers = confirm("Would you like to include numbers?");
+  var symbols = confirm("Would you like to include special characters?");
 
 
+}
+
+// Creating loop for password
+for(var i=0; i)
+
+// Values 
+let uppercasevalue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let lowercasevalue = "abcdefghijklmnopqrstuvwxyz"
+let numbervalue = "0123456789"
+let symbolvalue = "!@#$%^&*()"
+
+
+// Password to generate to textbox
+document.getElementById("password").value = password
